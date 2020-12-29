e51f87ecdfd7bdfc9c3c35822188a769a201fe3d
JPEdgar <sevisin@hotmail.com>
2020-12-29 00:53:23 -0800

    need to work on key

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -8,17 +8,48 @@ let tempVar = 0;
 
 export default function Grid() {
   const [getGrid, setGrid] = useState(() => InitializeGrid());
+  const [getCount, setCount] = useState(0);
   console.log(getGrid);
 
   function TestFunction() {
-    let temp = getGrid[0].props.children;
-     console.log(getGrid[1])
+    let tempID = 0;
+    let i = 0;
+    let j = 0;
 
+    let tempArr = [...getGrid];
+    const locArr = [];
+
+    const anotherTemp = tempArr.filter((tempItem) => {
+      let moreTemp = tempItem.props.id;
+      if (moreTemp === getCount) {
+        locArr[0] = parseInt(tempItem.props.children);
+        locArr[1] = parseInt(
+          tempItem.props.children.slice(locArr[0].length + 1)
+        );
+      }
+      return moreTemp !== getCount;
+    });
+
+    while (i >= GridDetails.numRows) {
+      i -= GridDetails.numRows;
+      j++;
+    }
+
+    anotherTemp.push(BuildCell(getCount, j, "visitedNode", tempID));
+    // console.log(anotherTemp.length);
+    anotherTemp.sort((a, b) => {
+      return a.props.id - b.props.id;
+      // console.log(parseInt(a.props.children));
+    });
+    // console.log(parseInt(tempLoc));
+    // console.log(anotherTemp);
+    setCount((count) => count + 1);
+    setGrid(anotherTemp);
   }
 
   function PrintGrid() {
-    console.log(getGrid);
     console.log(" - - - ");
+    console.log(getGrid);
   }
   return (
     <>
@@ -30,17 +61,19 @@ export default function Grid() {
 }
 
 function InitializeGrid() {
+  let tempVar = 0;
   const gridArray = [];
   for (let i = 0; i < GridDetails.numCols; i++) {
     for (let j = 0; j < GridDetails.numRows; j++) {
-      gridArray.push(BuildCell(i, j, "unvisitedNode"));
+      gridArray.push(BuildCell(i, j, "unvisitedNode", tempVar));
       // console.log(gridArray)
+      tempVar++;
     }
   }
   return gridArray;
 }
 
-function BuildCell(i, j, className) {
+function BuildCell(i, j, className, id) {
   let tempClass = className;
 
   const HandleClick = (e) => {
@@ -68,6 +101,7 @@ function BuildCell(i, j, className) {
 
   return (
     <div
+      id={id}
       key={`${i}, ${j}`}
       onClick={(e) => HandleClick(e)}
       className={`gridCell ${tempClass}`}
